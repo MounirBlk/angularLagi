560ab07c6f450a052ec99f609bab41a51751eb71
MounirBlk <mounir.ballouk@gmail.com>
2020-10-03 22:10:11 +0200

    Avancement conditions; boucles, model, binding, ternaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  name = "Mounir";
+  boolTest = true;// false = success / true = danger
+  danger = 'text-danger'
+  success ='text-success'
+  title=null;
+  colorClasses= {
+    "text-success" : !this.boolTest,
+    "text-danger" : this.boolTest,
+  }
+  titleStyles={
+    color:"orange",
+    fontStyle:"italic"
+  }
+  lettre="A"//a,b,c ou A,B,C
+  listPays=[
+    {id:1, name:'France'},
+    {id:2, name:'Allemagne'},
+    {id:3, name:'Italie'},
+    {id:4, name:'Espagne'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("onInit")
   }
 
+  GetUser() {
+    return "Hello" + this.name;
+  }
+  onClickBtn(event = null){
+    console.log(event)
+    this.title= this.name + " said: " + event;
+  }
 }
